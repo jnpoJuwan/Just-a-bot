b8c865b88064b52b2b8f304a87d3c76afcaf10b8
jnpoJuwan <48970145+jnpoJuwan@users.noreply.github.com>
2020-12-10 19:13:55 -0300

    Update spam.py

--- cogs/spam.py
+++ cogs/spam.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 
+from just_a_bot.utils import exceptions
+
 # This module is separate from the 'fun' module
 # for the bot owners to be able to unload the spam alone, and not break other commands.
 
@@ -18,11 +20,11 @@ class Spam(commands.Cog):
     @commands.cooldown(3, 60.0, commands.BucketType.user)
     async def spam(self, ctx, amount=5, *, message="spam"):
         """Spam a message an amount of times."""
-        if amount > SPAM_LIMIT:
-            await ctx.send(f"That's too much spam. The amount can't exceed {SPAM_LIMIT}.")
-        else:
-            for i in range(amount):
+        if amount < SPAM_LIMIT:
+            for _ in range(amount):
                 await ctx.send(message)
+        else:
+            await ctx.send(f"That's too much spam. The amount can't exceed {SPAM_LIMIT}.")
 
     # Exception Handling.
 
@@ -30,6 +32,7 @@ class Spam(commands.Cog):
     async def spam_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
             await ctx.send("Insert the spam amount and, optionally, a message, in this order.")
+
 
 
 def setup(bot):
