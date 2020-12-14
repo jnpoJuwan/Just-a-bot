e2817f31d8231df863657327c97b78a9f91dbdf2
jnpoJuwan <48970145+jnpoJuwan@users.noreply.github.com>
2020-12-14 11:26:38 -0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -3,7 +3,7 @@ import json
 import discord
 from discord.ext import commands
 
-from just_a_bot.cogs.embeds import COLOUR
+from .embeds import COLOUR
 
 
 class Help(commands.Cog):
@@ -34,7 +34,6 @@ class Help(commands.Cog):
                                 "playing around with creating a bot for some "
                                 "[Just a chat...](https://aminoapps.com/c/conlang-conscript/home/)-related servers.",
                     colour=COLOUR)
-                embed.add_field(name="Source Code", value="https://github.com/jnpoJuwan/just_a_bot", inline=False)
 
             if section == 1:
                 async with ctx.typing():
@@ -50,7 +49,7 @@ class Help(commands.Cog):
                                     f"►`{p}spam [amount] [message]` spams a message an given amount of times.\n"
                                     f"►`{p}umlaut [message]` sënds yöür sëntëncë wïth ümläüts.\n"
                                     f"►`{p}ping` Pong.",
-                        colour=discord.Colour(0x8b0000))
+                        colour=COLOUR)
                     embed_ext.add_field(name="Action Commands",
                                         value=f"►`{p}kiss [member]` kisses the member.\n"
                                               f"►`{p}fuck [member]` fucks the member's arse.\n"
@@ -60,25 +59,27 @@ class Help(commands.Cog):
                 async with ctx.typing():
                     embed_ext = discord.Embed(
                         title="Administration Commands (2/3)",
-                        description=f"►`{p}ban [@member] (reason)` bans the member.\n"
-                                    f"►`{p}kick [@member] (reason)` kicks the member.\n"
+                        description=f"►`{p}ban <@member> (reason)` bans the member.\n"
+                                    f"►`{p}kick <@member> (reason)` kicks the member.\n"
                                     f"►`{p}change_prefix [prefix]` changes the server's prefix.\n"
                                     f"►`{p}purge [amount]` (or `{p}clear (num)`) purges the amount of messages.\n"
-                                    f"►`{p}unban [user_id]` unbans the user.",
-                        colour=discord.Colour(0x8b0000))
+                                    f"►`{p}unban <user_id>` unbans the user.",
+                        colour=COLOUR)
             elif section == 3:
                 async with ctx.typing():
                     embed_ext = discord.Embed(
                         title="Utility Commands (3/3)",
                         description=f"►`{p}coin_flip [amount]` flips a coin an amount of times.\n"
+                                    f"►`{p}eval [command]` evaluates the Python code."
                                     f"►`{p}get_prefix` sends the server's prefix.\n"
                                     f"►`{p}length [message]` sends the message's length.\n"
+                                    f"►`{p}poll [question]` creates a poll for a question.\n"
                                     f"►`{p}random` sends a fractional random number between 0 and 1.\n"
                                     f"►`{p}roll [die] [amount]` rolls a die of a input number, "
                                     f"which is 20 by default, an amount of times.\n"
-                                    f"►`{p}poll [question]` creates a poll for a question.\n"
+                                    f"►`{p}source_code` sends the link to the bot's GitHub repository."
                                     f"►`{p}words [message]` sends the number of words in the message.",
-                        colour=discord.Colour(0x8b0000))
+                        colour=COLOUR)
                     embed_ext.add_field(
                         name="Information Commands",
                         value=f"►`{p}member [@member]` sends the member's information.\n"
@@ -106,6 +107,7 @@ class Help(commands.Cog):
     async def info_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
             await ctx.send("Please insert a positive integer less or equal to 3.")
+
 
 def setup(bot):
     bot.add_cog(Help(bot))
