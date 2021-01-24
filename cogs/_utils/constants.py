9a82ebc9f7cc0916ca3f62de7e78a8628a154b22
jnpoJuwan <48970145+jnpoJuwan@users.noreply.github.com>
2021-01-24 10:52:00 -0300

    Update minor changes

--- cogs/_utils/constants.py
+++ cogs/_utils/constants.py
@@ -2,7 +2,8 @@ import os
 
 import discord
 
+COGS = [f'cogs.{module[:-3]}' for module in os.listdir('cogs') if module.endswith('.py')
+        and not module.startswith('_')]
 COLOUR = discord.Colour(0x8b0000)
-COGS = [module for module in os.listdir('cogs') if module.endswith('.py') and not module.startswith('_')]
 DEFAULT_PREFIX = '?'
 SPAM_LIMIT = 25
