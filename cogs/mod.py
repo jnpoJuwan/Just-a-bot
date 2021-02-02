f993a47cf3daf075b7e53ceb5523b99f3bb96c14
jnpoJuwan <48970145+jnpoJuwan@users.noreply.github.com>
2021-02-02 08:52:04 -0300

    Create mod.py

---
+++ cogs/mod.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+
+from ._utils import checks
+
+
+class Mod(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	@checks.is_mod()
+	async def mute(self, ctx, member: discord.Member):
+		"""Mute the member."""
+		await member.edit(mute=True)
+		await ctx.send(f'{member.display_name} has been muted.')
+
+	@commands.command(aliases=['clear'])
+	@commands.cooldown(3, 60.0, commands.BucketType.user)
+	@checks.is_mod()
+	async def purge(self, ctx, amount=0):
+		"""Purge the given amount of messages."""
+		limit = 200
+		if amount > limit:
+			await ctx.send(f'The amount can\'t exceed {limit} messages.')
+			return
+
+		await ctx.message.delete()
+		await ctx.channel.purge(limit=amount)
+		await ctx.send(f'Purged {amount} message(s).', delete_after=2.5)
+
+	@purge.error
+	async def purge_error(self, ctx, error):
+		if isinstance(error, commands.BadArgument):
+			await ctx.send('Please enter a positive integer.')
+
+	@commands.command()
+	@checks.is_mod()
+	async def unmute(self, ctx, member: discord.Member):
+		"""Unmute the member."""
+		await member.edit(mute=False)
+		await ctx.send(f'{member.display_name} has been unmuted.')
+
+	@mute.error
+	@unmute.error
+	async def member_error(self, ctx, error):
+		if isinstance(error, commands.BadArgument):
+			await ctx.send('Please @mention a member.')
+
+
+def setup(bot):
+	bot.add_cog(Mod(bot))
