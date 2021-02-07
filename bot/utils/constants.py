4a57dfddf150db22edfd6e9378e8dc420f2f3807
jnpoJuwan <48970145+jnpoJuwan@users.noreply.github.com>
2021-02-07 09:53:12 -0300

    Recreate constants.py

---
+++ bot/utils/constants.py
@@ -0,0 +1,5 @@
+import discord
+
+COLOUR = discord.Colour(0x8b0000)
+DEFAULT_PREFIX = '?'
+SPAM_LIMIT = 25
--- cogs/_utils/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import discord
-
-COGS = [f'cogs.{module[:-3]}' for module in os.listdir('cogs') if module.endswith('.py')
-        and not module.startswith('_')]
-COLOUR = discord.Colour(0x8b0000)
-DEFAULT_PREFIX = '?'
-SPAM_LIMIT = 25
