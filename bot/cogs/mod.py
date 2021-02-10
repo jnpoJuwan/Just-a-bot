8f07130c151104c0a59d50a01d0027cc6ddd3644
jnpoJuwan <48970145+jnpoJuwan@users.noreply.github.com>
2021-02-10 16:18:33 -0300

    Change logic for ?purge

--- bot/cogs/mod.py
+++ bot/cogs/mod.py
@@ -15,24 +15,31 @@ class Mod(commands.Cog):
 		await member.edit(mute=True)
 		await ctx.send(f'{member.display_name} has been muted.')
 
-	@commands.command(aliases=['clear'])
+	@commands.command(aliases=['clear', 'delete'])
 	@commands.cooldown(3, 60.0, commands.BucketType.user)
 	@checks.is_mod()
-	async def purge(self, ctx, amount=0):
+	async def purge(self, ctx, amount=0, member: discord.Member = None):
 		"""Purges the amount of messages."""
 		limit = 200
 		if amount > limit:
 			await ctx.send(f'The amount can\'t exceed {limit} messages.')
 			return
 
-		await ctx.message.delete()
-		await ctx.channel.purge(limit=amount)
-		await ctx.send(f'Purged {amount} message(s).', delete_after=2.5)
+		if not member:
+			await ctx.message.delete()
+			await ctx.channel.purge(limit=amount)
+		else:
+			await ctx.message.delete()
+			channel = ctx.channel
+			await channel.purge(limit=amount, check=lambda message: message.author == ctx.author)
+		await ctx.send(f'Successfully purged {amount} message(s).', delete_after=2.5)
 
 	@purge.error
 	async def purge_error(self, ctx, error):
 		if isinstance(error, commands.BadArgument):
-			await ctx.send('Please enter a positive integer.')
+			await ctx.send(
+				'Please enter a positive integer and, optionally, a member whose messages you want deleted.'
+			)
 
 	@commands.command()
 	@checks.is_mod()
